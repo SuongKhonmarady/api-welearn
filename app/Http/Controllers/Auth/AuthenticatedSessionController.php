4e61b7152f109bf7a46f59e7545d50e08f74877f
Yoyo-abc <mardybor@gmail.comm>
2025-01-04 22:35:40 +0700

    Initial commit

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
-
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User; 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str; 
 
 class AuthenticatedSessionController extends Controller
 {
@@ -19,7 +21,22 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        $user = User::find(Auth::id()); 
+
+        // Generate a unique token
+        $token = Str::random(60); 
+
+        // Update the user's token in the database
+        // $user->api_token = Hash::make($token);
+        $user->api_token =$token; 
+        $user->save();
+
+        // Return no content and set the token in the response headers
+        return response()->noContent()->header('Authorization', 'token ' . $token );
+        // return response()->json([
+        //     'user' => $user,
+        //     'token' => $token 
+        // ]); 
     }
 
     /**
